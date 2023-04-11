1849591421165f793fe9972bc98716ce1333fe15
Fransedo Ravelino <fransedo.ravelino@BLUEBIRDGROUP.COM>
2023-04-11 11:50:49 +0700

    added loading states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { type RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import { LoadingPage } from "~/components/loading";
 
 dayjs.extend(relativeTime);
 
@@ -55,14 +56,26 @@ const PostView = (props: PostWithUser) => {
   );
 };
 
+const Feed = () => {
+  const { data, isFetched: postLoaded } = api.posts.getAll.useQuery();
+
+  if (!postLoaded) return <LoadingPage />;
+
+  if (!data) return <div>Something went wrong...</div>;
+  return (
+    <div className="flex flex-col">
+      {data?.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id}></PostView>
+      ))}
+    </div>
+  );
+};
+
 const Home: NextPage = () => {
-  const user = useUser();
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
 
-  const { data, isLoading } = api.posts.getAll.useQuery();
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (!data) return <div>Something went wrong</div>;
+  // Returns empty div if BOTH aren't loaded, since user tends to load faster
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -74,13 +87,9 @@ const Home: NextPage = () => {
       <main className="flex h-screen justify-center">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex justify-center border-b border-slate-400 p-4">
-            {user.isSignedIn ? <CreatePostWizard /> : <SignInButton />}
+            {isSignedIn ? <CreatePostWizard /> : <SignInButton />}
           </div>
-          <div className="flex flex-col">
-            {data?.map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id}></PostView>
-            ))}
-          </div>
+					<Feed />
         </div>
       </main>
     </>
